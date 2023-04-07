3fd5d1a6f1d0bd07504a791eb4e65475083ca128
Huy <trannhathuy1305@gmail.com>
2023-04-07 20:29:45 +0700

    Add particle background

--- src/App.js
+++ src/App.js
@@ -5,25 +5,13 @@ import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import 'tachyons'
-import Particles from 'react-particles-js'
+import ParticleBackground from './components/ParticleBackground/ParticleBackground'
 
 class App extends Component {
  render() {
   return (
    <div className="App">
-    <Particles
-     params={{
-      particles: {
-       line_linked: {
-        shadow: {
-         enable: true,
-         color: '#3CA9D1',
-         blur: 5,
-        },
-       },
-      },
-     }}
-    />
+    <ParticleBackground />
     <Navigation />
     <Logo />
     <Rank />
