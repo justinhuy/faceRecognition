3116dd4d72b73b9279ff1a3ee76af16ad7cc5376
Huy <trannhathuy1305@gmail.com>
2023-04-07 15:05:30 +0700

    Nav, Logo, Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
 import './App.css'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import 'tachyons'
+import Particles from 'react-particles-js'
 
 class App extends Component {
  render() {
-  return <div className="App"></div>
+  return (
+   <div className="App">
+    <Particles
+     params={{
+      particles: {
+       line_linked: {
+        shadow: {
+         enable: true,
+         color: '#3CA9D1',
+         blur: 5,
+        },
+       },
+      },
+     }}
+    />
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm />
+
+    {/* <FaceRecognition /> */}
+   </div>
+  )
  }
 }
 
